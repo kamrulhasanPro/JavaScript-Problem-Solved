480a41809f74e3d638e45df38a157f71d0b76513
kamrulmiazi <kamrul116522@gmail.com>
2026-01-08 16:19:51 +0600

    solved problem 2

--- problem-solved.js
+++ problem-solved.js
@@ -9,3 +9,22 @@ function reversedString(string) {
 const resultReversed = reversedString("monir");
 console.log(resultReversed);
 
+// problem - 2: Write a function that counts how many vowels (a, e, i, o, u) are in a given string.
+
+function countVowels(string) {
+  const vowels = "aeiou";
+  //   const count = string
+  //     .split("")
+  //     .filter((alphabet) => vowels.includes(alphabet.toLowerCase())).length;
+
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    const alphabet = string.toLowerCase()[i];
+    if (vowels.includes(alphabet)) {
+      count++;
+    }
+  }
+  return count;
+}
+const resultVowelsCount = countVowels("kamrul hasan");
+console.log(resultVowelsCount);
